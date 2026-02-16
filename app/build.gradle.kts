897623dd30b90e5a3dc34bc307966405ea467940
cobi-dev0615 <stanislavsokoloskyi44@gmail.com>
2026-02-16 10:09:02 -0600

    fix error 0216

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.seretail.inventarios"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
